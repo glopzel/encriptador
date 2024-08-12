c0c9f67f7c2deba053f18c2a7d595c3be950e969
Angela Lopez <angelalopez96@gmail.com>
2024-08-11 20:59:13 -0400

    Elimina comentario sobre el funcionamiento del encriptador en app.js

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ const cambiosEncriptador = {
     "u": "ufat"
 }; 
 
-/*
-Debe funcionar solo con letras minúsculas
-
-No deben ser utilizados letras con acentos ni caracteres especiales
-
-Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
-*/
 function encriptarTexto() {
     let textoInicial = textoParaProcesar.value;
     let textoFinal = "";
